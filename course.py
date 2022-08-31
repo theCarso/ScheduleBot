64d56c5211c053cf232dfb7118c6469890f4b83b
Carson Miller <miller.1653@wright.edu>
2022-08-31 18:18:10 -0400

    Commented course.py

--- course.py
+++ course.py
@@ -9,13 +9,18 @@ class Course:
         self.endmin=int(endmin)
         self.days=days
     
+    # Returns true if the class is currently happening
     def isInClass(self):
         now = datetime.datetime.now()
         starttime = now.replace(hour=self.starthour, minute=self.startmin, second=0, microsecond=0)
         endtime = now.replace(hour=self.endhour, minute=self.endmin, second=0, microsecond=0)
+        # returns true if it is after start time, before end time, and on a proper day
         return ((now > starttime) & (now < endtime) & (str(now.weekday()) in self.days))
     
+    # Returns the end time formatted the following:
+    # HH:MM PM  ||  HH:MM AM
     def getEndTimeString(self):
+        # yes i know this probably can be done shorter i'm new to python give me a break
         ampm = "AM"
         hour = self.endhour
         min = self.endmin
